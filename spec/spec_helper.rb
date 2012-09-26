51da25fc180a9aa2f72af0bfc036c7ab993f98a9
Rob Lyon <nosignsoflifehere@gmail.com>
2012-09-25 18:30:23 -0700

    fixed regex for older ruby

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require 'bundler/setup'
 require 'ldap_mapper'
 
 def check_password(password, ssha)
-  salt = Base64.decode64(ssha.gsub(/^{SSHA}/, ''))[20..-1]
+  salt = Base64.decode64(ssha.gsub(/^\{SSHA\}/, ''))[20..-1]
   Net::LDAP::Password.generate(:ssha, password, salt)
 end
 
